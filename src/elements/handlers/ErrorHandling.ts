30ba0ef62f8a4289780c0ae97959fba00da0b15b
Mased <68379695+MasedMSD@users.noreply.github.com>
2023-05-14 20:12:47 +0500

    Update ErrorHandling.ts

--- src/elements/handlers/ErrorHandling.ts
+++ src/elements/handlers/ErrorHandling.ts
@@ -4,8 +4,8 @@ export default new Handler({
 	name: "ErrorHandling",
 
 	run: async (): Promise<void> => {
-		// process.on("uncaughtExceptionMonitor", (error, p) => console.error(`${error} \n\n${p}`));
-		// process.on("uncaughtException", (error, p) => console.error(`${error} \n\n${p}`));
-		// process.on("unhandledRejection", (error, p) => console.error(`${error} \n\n${p}`));
+		process.on("uncaughtExceptionMonitor", (error, p) => console.error(`${error} \n\n${p}`));
+		process.on("uncaughtException", (error, p) => console.error(`${error} \n\n${p}`));
+		process.on("unhandledRejection", (error, p) => console.error(`${error} \n\n${p}`));
 	},
 });
